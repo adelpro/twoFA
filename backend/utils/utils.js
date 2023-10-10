2819dc6717272360eb3a0408a9f761f0046a2fa5
adelpro <adelpro@gmail.com>
2023-10-10 14:50:44 +0100

    fix

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,23 +1,10 @@
+const { authenticator } = require("otplib");
 const speakeasy = require("speakeasy");
-const {authenticator} = require("otplib");
 const qrcode = require("qrcode");
 async function generateTwoFAQRCode(id, secret) {
-/*   const otpauthURL = speakeasy.otpauthURL({
-    secret,
-    label: "2FA-auth",
-  }); */
-  const otpauthURL = authenticator.keyuri(id, "2FA-auth", secret);
-  const result = await qrcode.toDataURL(otpauthURL);
+  const label = id;
+  const otpauthURL = speakeasy.otpauthURL({});
+  const result = await qrcode.toDataURL(secret);
   return result;
 }
-function showCurrentTime() {
-  const currentTime = new Date();
-  const hours = currentTime.getHours();
-  const minutes = currentTime.getMinutes();
-  const seconds = currentTime.getSeconds();
-
-  const formattedTime = `${hours}:${minutes}:${seconds}`;
-
-  console.log(formattedTime);
-}
-module.exports = { generateTwoFAQRCode, showCurrentTime };
+module.exports = { generateTwoFAQRCode };
